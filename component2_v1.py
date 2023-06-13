898a14c5541b2f7acb208d48c5bb8300b0c1b7ad
gloriori <134894290+gloriori@users.noreply.github.com>
2023-06-13 13:53:08 +1200

    component2 v2

--- component2_v1.py
+++ component2_v1.py
@@ -15,3 +15,26 @@ def program_user():
 
 
 program_user()
+
+#  ask the user if they want to read instructions
+
+
+def rules_user():
+    show_rules = input("Would you like to read the instructions? ")
+    if show_rules == "":
+        print("Sorry, but this cannot be blank. Please try again.")
+        return rules_user()
+    elif show_rules == 'yes' or show_rules == 'Yes':
+        print('''\n 
+                *** Instructions ***
+    1st: Choose a cupcake flavour with the flavours that we have.
+    2nd: Choose a icing that would like with the flavours that we have.
+    3rd: Choose any toppings that would like to have with the options given''')
+    elif show_rules == 'no' or show_rules == 'No':
+        print("Okay, You may now start with building a cupcake!")
+    else:
+        print("You must respond with Yes/No!")
+        return rules_user()
+
+
+rules_user()
