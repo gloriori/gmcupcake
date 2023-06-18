97108c409525c2f2f37bcb787119e8a804af8a12
gloriori <134894290+gloriori@users.noreply.github.com>
2023-06-19 03:32:11 +1200

    Component 3 V1

--- component3_v1.py
+++ component3_v1.py
@@ -25,13 +25,6 @@ print('''\n
     Please choose carefully!
     ''')
 
-
-# Set flavours in three lists
-
-flavour_one = ['Vanilla', 'vanilla', 'Chocolate', 'chocolate', 'Red Velvet', 'red velvet', 'Red velvet', 'red Velvet']
-flavour_two = ['Carrot Cake', 'carrot cake', 'carrot Cake', 'Carrot cake', 'Strawberry', 'strawberry', 'Lemon', 'lemon']
-flavour_three = ['Coffee', 'coffee', 'Confetti', 'confetti']
-
 # Ask user what flavour they would like
 
 
@@ -40,7 +33,7 @@ def cupcake_flavour():
     if cake_flavours == "":
         print("Sorry this cannot be blank. Please try again.")
         return cupcake_flavour()
-    elif cake_flavours in flavour_one or flavour_two or flavour_three:
+    elif cake_flavours == 'Vanilla' or cake_flavours == 'vanilla' or cake_flavours == 'Chocolate' or cake_flavours == 'Red Velvet' or cake_flavours == 'red velvet' or cake_flavours == 'Red velvet' or cake_flavours == 'red Velvet' or cake_flavours == 'Carrot Cake' or cake_flavours == 'carrot cake'  or cake_flavours == 'Carrot cake' or cake_flavours == 'carrot Cake' or cake_flavours == 'Strawberry' or cake_flavours == 'strawberry' or cake_flavours == 'lemon' or cake_flavours == 'Lemon' or cake_flavours == 'Coffee' or cake_flavours == 'coffee' or cake_flavours == 'confetti' or cake_flavours == 'Confetti':
         print("I love that flavour too!")
     else:
         print("You may only enter options from the text above")
@@ -63,13 +56,9 @@ print('''\n
     7. Cream Cheese Frosting
     8. Lemon Buttercream
     \t
-    Please choose carefully and when you enter please enter' JUST the first word e.g Strawberry or Whipped!
+    Please choose carefully and when you enter please enter' JUST the first word e.g Strawberry or Whipped
     ''')
 
-# Set frostings into lists
-
-frosting_one = ['Strawberry', 'strawberry', 'Vanilla', 'vanilla', 'Chocolate', 'chocolate', 'Whipped', 'whipped']
-frosting_two = ['Caramel', 'caramel', 'Silky', 'silky', 'Cream', 'cream', 'Lemon', 'lemon']
 
 # Ask user what flavour they would like
 
@@ -79,7 +68,7 @@ def frosting_flavour():
     if frost_choice == "":
         print("Sorry this cannot be blank. Please try again.")
         return frosting_flavour()
-    elif frost_choice in flavour_one or flavour_two or flavour_three:
+    elif frost_choice == 'Strawberry' or frost_choice == 'strawberry' or frost_choice == 'Vanilla' or frost_choice == 'vanilla' or frost_choice == 'chocolate' or frost_choice == 'chocolate' or frost_choice == 'whipped' or frost_choice == 'Whipped' or frost_choice == 'caramel' or frost_choice == 'Caramel' or frost_choice == 'Silky' or frost_choice == 'silky' or frost_choice == 'cream' or frost_choice == 'Cream' or frost_choice == 'Lemon' or frost_choice == 'lemon':
         print("That is an amazing choice!")
     else:
         print("You may only enter options from the text above")
@@ -87,3 +76,41 @@ def frosting_flavour():
 
 
 frosting_flavour()
+
+# Introduce toppings to user
+
+print("Now that you have picked the frosting here are the toppings available!")
+print('''\n 
+                *** Toppings ***
+    1. Rainbow Sprinkles
+    2. Chocolate Sprinkles
+    3. Cherries
+    4. Sugar Pearls
+    5. Marshmallows
+    6. Strawberries
+    7. Chocolate Drizzle
+    8. Caramel Drizzle
+    9. None
+    \t
+    Please enter the first word of the topping you want eg. Sugar or Rainbow! When it comes to Chocolate Sprinkles or 
+    Chocolate Drizzle, enter it's full name!
+    ''')
+
+# ask the user what toppings they would like
+
+
+def toppings_choice():
+    top_choice = input("What topping would you like to choose? ")
+    if top_choice == "":
+        print("Sorry this cannot be blank. Please try again.")
+        return toppings_choice()
+    elif top_choice == 'None' or top_choice == 'none':
+        print("That is okay, we may now calculate the final price of your cupcakes.")
+    elif top_choice == 'Rainbow' or top_choice == 'rainbow' or top_choice == 'chocolate sprinkles' or top_choice == 'Chocolate Sprinkles' or top_choice == 'Chocolate sprinkles' or top_choice == 'chocolate Sprinkles' or top_choice == 'Cherries' or top_choice == 'cherries' or top_choice == 'Sugar' or top_choice == 'sugar' or top_choice == 'Marshmallows' or top_choice == 'marshmallows' or top_choice == 'strawberries' or top_choice == 'Strawberries' or top_choice == 'Chocolate Drizzle' or top_choice == 'chocolate drizzle' or top_choice == 'chocolate Drizzle' or top_choice == 'Chocolate drizzle' or top_choice == 'caramel' or top_choice == 'Caramel':
+        print("That is a fantastic choice! We may now calculate the final price of your cupcakes.")
+    else:
+        print("You may only enter options from the text above")
+        return toppings_choice()
+
+
+toppings_choice()
